1bc4308ad3b147436c096d1c0ecba10ab74cf559
Brent <brent.markus@gmail.com>
2018-03-13 10:19:46 -0500

    changes

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -5,10 +5,14 @@ print(sys.version)
 for i in range(0, 100):
     output = ""
 
+    if(i % 2 == 0):
+        output += 'Even'
     if(i % 3 == 0):
         output += 'Fizz'
     if(i % 5 == 0):
         output += 'Buzz'
+    if(i % 7 == 0):
+        output += 'Sevr'
     if(output == ''):
         output = i
 
---
+++ Prime.py
@@ -0,0 +1,12 @@
+def isPrime(n):
+    if n==2 or n==3: return True
+    if n%2==0 or n<2: return False
+    for i in range(3,int(n**0.5)+1,2):   # only odd numbers
+        if n%i==0:
+            return False    
+
+    return True
+    
+for i in range(10**3,10**4):
+	if(isPrime(i)):
+		print(i,' is prime')
