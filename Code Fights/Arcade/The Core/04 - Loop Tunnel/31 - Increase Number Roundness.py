4b3f83662f3bf00dc5ba03a0db65306fb79fa2b0
Brent Markus <brent.markus@gmail.com>
2018-05-06 13:11:59 -0500

    arcade

--- Code Fights/Arcade/The Core/04 - Loop Tunnel/31 - Increase Number Roundness.py
+++ Code Fights/Arcade/The Core/04 - Loop Tunnel/31 - Increase Number Roundness.py
@@ -1,1 +1,13 @@
 def increaseNumberRoundness(n):
+    while n % 10 == 0:
+        n = n // 10
+    while n >= 1:
+        if n % 10 == 0:
+            return True
+        n = n // 10
+    return False
+
+
+# simpler:
+'''def increaseNumberRoundness(n):
+    return '0' in str(n).rstrip('0')'''
