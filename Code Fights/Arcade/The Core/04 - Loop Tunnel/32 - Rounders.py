c262174a26999f146f2ef3a0ab1fb2cee0e25152
Brent Markus <brent.markus@gmail.com>
2018-05-17 15:49:25 -0500

    arcade

--- Code Fights/Arcade/The Core/04 - Loop Tunnel/32 - Rounders.py
+++ Code Fights/Arcade/The Core/04 - Loop Tunnel/32 - Rounders.py
@@ -1,1 +1,20 @@
+import math
+
+
 def rounders(value):
+
+    order = math.floor(math.log10(value)) + 1
+    for i in range(1, order):
+        if value % 10 < 5:
+            value = value // 10
+        else:
+            value = value // 10 + 1
+    return value * 10 ** (order - 1)
+
+
+print(rounders(15))
+print(rounders(1234))
+print(rounders(1445))
+print(rounders(14))
+print(rounders(10))
+print(rounders(99))
