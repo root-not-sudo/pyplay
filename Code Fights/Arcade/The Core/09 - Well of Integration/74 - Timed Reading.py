0c1c1973e9d287dee537438fc3602691999912fc
Brent Markus <brent.markus@gmail.com>
2018-09-01 12:27:57 -0500

    arcade

--- Code Fights/Arcade/The Core/09 - Well of Integration/74 - Timed Reading.py
+++ Code Fights/Arcade/The Core/09 - Well of Integration/74 - Timed Reading.py
@@ -3,11 +3,14 @@ def timedReading(maxLength, text):
     wordlen = []
     letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
     j = 0
-    for i in text:
-        if i in letters:
+    for i in range(len(text)):
+        if text[i] in letters:
+            print(i)
             j += 1
         else:
             if j > 0 and j <= maxLength:
                 wordlen.append(j)
             j = 0
+    if j > 0 and j <= maxLength:
+        wordlen.append(j)
     return len(wordlen)
