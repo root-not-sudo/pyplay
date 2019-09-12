bb3093514de348a3e4725343f218f5838845cd4a
Brent <brent.markus@gmail.com>
2019-09-11 22:00:18 -0500

    craps

---
+++ craps.py
@@ -0,0 +1,30 @@
+"""1st roll wins on 7, 11 -- loses on 2, 3, 12 -- store any other roll
+rolls after that stored roll wins -- loses on 7 -- keep rolling on anything else"""
+import random
+
+def diceRoll(sides):
+    return random.randint(1,sides)
+
+def craps():
+    """1st Roll"""
+    die1, die2 = diceRoll(6), diceRoll(6)
+    firstRoll=die1+die2
+    print(f'Welcome to craps \nWith a {die1} and a {die2} you rolled a {firstRoll}')
+    if firstRoll in [7, 11]:
+        print('You Win!')
+    if firstRoll in [2,3,12]:
+        print('You Lose.')
+    if firstRoll not in [2, 3, 7, 11, 12]:
+        subsequentRoll = 0
+        while(firstRoll != subsequentRoll and subsequentRoll != 7):
+            die1, die2 = diceRoll(6), diceRoll(6)
+            subsequentRoll=die1+die2
+            input(f'point. You must roll a {firstRoll} to win. Press Enter to roll.')
+            print(f'\nWith a {die1} and a {die2} you rolled a {subsequentRoll}')
+        if firstRoll == subsequentRoll:
+            print('You Win!')
+        if subsequentRoll == 7:
+            print('You Lose.')
+    """Subsequent Rolls"""
+
+craps()
