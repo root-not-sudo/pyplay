11a950d165ef00e1284aa228143e3ba3ca040abe
Brent Markus <brent.markus@gmail.com>
2021-04-15 14:34:55 -0500

    playing around with == vs is

--- isVs==.py
+++ isVs==.py
@@ -22,3 +22,22 @@ print('b is c:', b is c)
 print('b == c:', b == c)
 print('d is e:', d is e)
 print('d == e:', d is e)
+
+x = 'Earth is a planet'
+y = 'Earth is a planet'
+y = 30
+y = "Earth is a planet"
+z = 5
+w = 5.0
+p = 5 / 1
+print(x == y)
+print(x is y)
+print(z == w)
+print(z is w)
+print(p == w)
+print(p is w)
+print(hex(id(x)))
+print(hex(id(y)))
+print(hex(id(z)))
+print(hex(id(w)))
+print(hex(id(p)))
---
+++ scratch_pad.py
@@ -0,0 +1,18 @@
+x = 'Earth is a planet'
+y = 'Earth is a planet'
+y = 30
+y = "Earth is a planet"
+z = 5
+w = 5.0
+p = 5 / 1
+print(x == y)
+print(x is y)
+print(z == w)
+print(z is w)
+print(p == w)
+print(p is w)
+print(hex(id(x)))
+print(hex(id(y)))
+print(hex(id(z)))
+print(hex(id(w)))
+print(hex(id(p)))
